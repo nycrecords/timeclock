813db5dff183e00dd1370bc5ce5fd52d35ee0b28
SarvarD <sdhillon4939@gmail.com>
2016-06-06 15:53:23 -0400

    base structure initialization (implementing blueprints)

---
+++ app/main/forms.py
@@ -0,0 +1,5 @@
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
+
---
+++ app/main/views.py
@@ -0,0 +1,10 @@
+from flask import render_template, session, redirect, url_for, current_app
+from .. import db
+from ..models import User
+from ..email import send_email
+from . import main
+#from .forms import <FORM NAME HERE>
+
+@main.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
