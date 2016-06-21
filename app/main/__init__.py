f68353e2eb43bde99165cf3d922b5a5b93552094
SarvarD <sdhillon4939@gmail.com>
2016-06-21 12:17:34 -0400

    add session timeout (15 minutes of inactivity)

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, session
 from datetime import timedelta
 
 main = Blueprint('main', __name__)
@@ -6,8 +6,14 @@ main = Blueprint('main', __name__)
 from . import views, errors
 from ..models import Permission
 
+
 @main.app_context_processor
 def inject_permissions():
     return dict(Permission=Permission)
 
-main.permanent_session_lifetime = timedelta(days=5)
+main.permanent_session_lifetime = timedelta(minutes=15)
+
+
+@main.before_request
+def func():
+    session.modified = True
