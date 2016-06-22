796b6d5732d394815ddea060efe40ea8f63b3809
SarvarD <sdhillon4939@gmail.com>
2016-06-22 16:15:41 -0400

    add debug statement to manage.py

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ from app.models import User, Role, Event, Pay, Tag, Password
 from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
 
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+app = create_app(os.getenv('FLASK_CONFIG') or 'default', debug=True)
 manager = Manager(app)
 migrate = Migrate(app, db)
 
