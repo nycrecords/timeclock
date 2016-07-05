f3350fee2f1bb0ab3e9dfef0153e762a87f914ec
aot221 <aot221@nyu.edu>
2016-07-05 14:48:32 -0400

    remove cors

--- app/__init__.py
+++ app/__init__.py
@@ -45,7 +45,6 @@ def create_app(config_name):  # App Factory
     mail.init_app(app)
     moment.init_app(app)
     migrate.init_app(app, db)
-    CORS.init_app(app)
     db.init_app(app)
     with app.app_context():
         load_db(db)
