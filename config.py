cb301c574c7d489357f264be5e7ac49b96174330
SarvarD <sdhillon4939@gmail.com>
2016-07-05 17:56:55 -0400

    development config has debug enabled

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ class Config:
 
 
 class DevelopmentConfig(Config):
-    DEBUG = False
+    DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'postgresql://localhost:5432/timeclock_dev'
 
