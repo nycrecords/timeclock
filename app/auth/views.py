c659b7568bbc62a49d521c1354c032ef021a4972
SarvarD <sdhillon4939@gmail.com>
2016-07-05 18:16:15 -0400

    Added debug statements for heroku

--- app/auth/views.py
+++ app/auth/views.py
@@ -140,7 +140,7 @@ def login():
                 flash('Your password will expire in {} days.'.format(days_to_expire), category='warning')
             current_app.logger.error('{} is still logged in. Redirecting to main.index'.format(current_user.email))
             # return redirect(request.args.get('next') or url_for('main.index'))
-            return url_for('main.index')
+            return render_template('index.html')
         if user:
             current_app.logger.info('{} failed to log in: Invalid username or password'.format(user.email))
             user.login_attempts += 1
