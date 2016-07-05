51b69a87361b72bf7c65fef90863e2a4992bf33c
SarvarD <sdhillon4939@gmail.com>
2016-07-05 18:29:33 -0400

    Added debug statements for heroku

--- app/auth/views.py
+++ app/auth/views.py
@@ -140,7 +140,7 @@ def login():
                 days_to_expire = (datetime.today() - current_user.password_list.last_changed).days
                 flash('Your password will expire in {} days.'.format(days_to_expire), category='warning')
             current_app.logger.error('{} is still logged in. Redirecting to main.index'.format(current_user.email))
-            return redirect(request.args.get('next') or url_for('main.index'))
+            # return redirect(request.args.get('next') or url_for('main.index'))
             # current_app.logger.info('{}'.format(request.args.get('next')))
             return index()
 
