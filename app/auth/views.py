ec6d73427bc5da4bceec89184cdd6c80d68c6b4e
SarvarD <sdhillon4939@gmail.com>
2016-07-06 14:28:23 -0400

    write registered user emails to db in lowercase

--- app/auth/views.py
+++ app/auth/views.py
@@ -37,7 +37,7 @@ def register():
     """
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(email=form.email.data,
+        user = User(email=(form.email.data).lower(),
                     password=form.password.data,
                     first_name=form.first_name.data,
                     last_name=form.last_name.data,
@@ -68,7 +68,7 @@ def admin_register():
         tag_id = 6
         if 'tag' in form:
             tag_id = form.tag.data
-        user = User(email=form.email.data,
+        user = User(email=(form.email.data).lower(),
                     password=temp_password,
                     first_name=form.first_name.data,
                     last_name=form.last_name.data,
