e72aaa5b8bdafba477751aa372147e0c725ef93b
SarvarD <sdhillon4939@gmail.com>
2016-07-07 14:54:58 -0400

    fix timesheet generation pop() issues

--- app/main/pdf.py
+++ app/main/pdf.py
@@ -75,9 +75,9 @@ def generate_timetable(canvas_field, events):
     canvas_field.line(20, timetable_top - 10, 600, timetable_top - 10)
 
     canvas_field.setFont('Courier', 10)
+    if len(events) % 2 != 0:
+        events.pop(0)
     events = sorted(events)
-    if not len(events) % 2 == 0:
-        events.pop()
     next_line = timetable_top
     for x in range(0, len(events), 2):
         event = events[x]
