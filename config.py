a23bf41ab083b5377c1b368254a52ee809e04b60
ImSimonG <sgong9570@gmail.com>
2017-06-26 09:10:44 -0400

    edited config.py

--- config.py
+++ config.py
@@ -8,10 +8,10 @@ class Config:
     MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'localhost'
     MAIL_PORT = os.environ.get('MAIL_PORT') or 1111
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') or False
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or None
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or None
     MAIL_SUBJECT_PREFIX = '[TimeClock]'
-    MAIL_SENDER = 'Records Timeclock <RTimeclock@records.nyc.gov>'
+    MAIL_SENDER = os.environ.get('MAIL_SENDER') or 'Records Timeclock <RTimeclock@records.nyc.gov>'
     ADMIN = os.environ.get('ADMIN') or 'admin@records.nyc.gov'
 
     @staticmethod
