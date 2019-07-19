5230ce1dc4a8fb503573762b8a3a1e546cf5f8b2
Danish Faruqi <dfaruqi@records.nyc.gov>
2019-07-19 11:48:08 -0400

    tried fixig inactive users issue

--- app/main/views.py
+++ app/main/views.py
@@ -615,13 +615,16 @@ def user_list_page():
         entry = request.args.get('search_input', '').lower()
         search_result_email = User.query.filter(User.email.like('%' + entry + '%')).all()
         print('email\n', search_result_email)
-        search_result_fname = User.query.filter(User.first_name.like('%' + entry.title() + '%')).all()
+        search_result_fname = User.query.filter(User.first_name.like('%' + entry.title())).all()
         print('fname\n', search_result_fname)
         search_result_lname = User.query.filter(User.last_name.like('%' + entry.title() + '%')).all()
         print('lname\n', search_result_lname)
-
         list_of_users = list(set(list_of_users_all) & set(search_result_email+search_result_fname+search_result_lname))
         print('result\n', list_of_users)
+
+# how to run a double quesry 
+
+    for user in list_of_users:
         
         if not list_of_users:
             flash('No results found', category = 'error')
