76bb3f871de110d7f2ee786910ded29d678647de
Munif Nagi <mnagi@records.nyc.gov>
2019-09-03 09:33:29 -0400

    fixed the formatting

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,7 +1,12 @@
 from flask_wtf import Form
 from wtforms import StringField, PasswordField, SubmitField, ValidationError, SelectField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo ,Optional
-
+from wtforms.validators import (
+    DataRequired,
+    Length,
+    Email,
+    EqualTo,
+    Optional
+)
 from ..models import User
 from ..utils import tags, divisions, roles
 
