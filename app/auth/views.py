b0ce6972fa72710d570a744dd48663bf1fd293d0
phurpatshering79 <psherpa@records.nyc.gov>
2019-09-12 17:04:20 -0400

    minor fix with const variable

--- app/auth/views.py
+++ app/auth/views.py
@@ -116,7 +116,7 @@ def login():
                     return redirect(url_for('auth.change_password'))
                 elif (datetime.today() - current_user.password_list.last_changed).days > passwords.DAYS_UNTIL_PW_WARNING:
                     # If user's password is about to expire (not updated in 75 days)
-                    days_to_expire = DAYS_TILL_EXPIRE-((datetime.today() - current_user.password_list.last_changed).days)
+                    days_to_expire = passwords.DAYS_TILL_EXPIRE-((datetime.today() - current_user.password_list.last_changed).days)
                     flash('Your password will expire in {} days.'.format(days_to_expire), category='warning')
                 current_app.logger.error('{} is already logged in. Redirecting to main.index'.format(current_user.email))
                 current_app.logger.info('End function login() [VIEW]')
